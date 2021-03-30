340fec317c3b7f39ef301a873c4ac6537406a4bb
Exterior7 <exteriorbwin@gmail.com>
2021-03-30 10:20:37 +0200

    routing works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TeacherComponent } from './pages/teacher/teacher.component';
+import { StudentComponent } from './pages/student/student.component';
+
+const routes: Routes = [
+
+  {
+    path: '',
+    component: TeacherComponent,
+  },
+  {
+    path: 'students',
+    component: StudentComponent,
+  },
+  {
+    path: '**',
+    component: TeacherComponent,
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
